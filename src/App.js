3cc6f208e827df7f7e4c6551b0548990ee7fabe3
Rob Halff <rob.halff@gmail.com>
2016-09-19 08:36:07 +0200

    cleanup event listeners

--- src/App.js
+++ src/App.js
@@ -568,6 +568,56 @@ export default class TheGraphApp extends Component {
     }, 500)
   }
 
+  componentWillUnmount () {
+    let hammertime
+    const {onNodeSelection} = this.props
+
+    const domNode = findDOMNode(this)
+
+    // Unselect edges and nodes
+    if (onNodeSelection) {
+      domNode.removeEventListener('tap', this.unselectAll)
+    }
+
+    if (Hammer) {
+      hammertime = new Hammer(domNode, {})
+      hammertime.get('pinch').set({ enable: false })
+
+      const isTouchDevice = 'ontouchstart' in document.documentElement
+
+      if (isTouchDevice && hammertime) {
+        hammertime.off('pinchstart', this.onTransformStart)
+        hammertime.off('pinch', this.onTransform)
+        hammertime.off('pinchend', this.onTransformEnd)
+      }
+    }
+
+    // Pointer gesture event for pan
+    domNode.removeEventListener('trackstart', this.onTrackStart)
+
+    // Wheel to zoom
+    if (domNode.onwheel !== undefined) {
+      // Chrome and Firefox
+      domNode.removeEventListener('wheel', this.onWheel)
+    } else if (domNode.onmousewheel !== undefined) {
+      // Safari
+      domNode.removeEventListener('mousewheel', this.onWheel)
+    }
+
+    // Tooltip listener
+    domNode.removeEventListener('the-graph-tooltip', this.changeTooltip)
+    domNode.removeEventListener('the-graph-tooltip-hide', this.hideTooltip)
+
+    // Edge preview
+    domNode.removeEventListener('the-graph-edge-start', this.edgeStart)
+
+    domNode.removeEventListener('contextmenu', this.onShowContext)
+
+    // HACK metaKey global for taps https://github.com/Polymer/PointerGestures/issues/29
+    document.removeEventListener('keydown', this.keyDown)
+    document.removeEventListener('keyup', this.keyUp)
+  }
+
   onShowContext (event) {
     event.preventDefault()
     event.stopPropagation()
@@ -666,9 +716,9 @@ export default class TheGraphApp extends Component {
     /*
      // HACK metaKey global for taps https://github.com/Polymer/PointerGestures/issues/29
      if (TheGraph.metaKeyPressed) {
-       TheGraph.metaKeyPressed = false;
+     TheGraph.metaKeyPressed = false;
      }
-    */
+     */
   }
 
   unselectAll (/* event */) {
