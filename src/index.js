f122bd24d4d6237630659c8141e069b7f70231f6
Rob Halff <rob.halff@gmail.com>
2016-09-21 02:28:15 +0200

    fix index

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 // Api
-export App from './src/App',
-export Clipboard from './src/Clipboard',
-export Edge from './src/Edge',
-export FONT_AWESOME from './src/config/FONT_AWESOME',
-export Graph from './src/Graph',
-export Group from './src/Group',
-export IIP from './src/Iip',
-export Menu from './src/Menu',
-export ModalBG from './src/ModalBG',
-export Node from './src/Node',
-export NodeMenu from './src/NodeMenu',
-export NodeMenuPort from './src/NodeMenuPort',
-export NodeMenuPorts from './src/NodeMenuPorts',
-export Port from './src/Port',
-export SVGImage from './src/common/SVGImage',
-export TextBG from './src/common/TextBG',
-export Tooltip from './src/Tooltip',
-export arcs from './src/factories/menu/arcs',
-// export config from './src/config',
-export contextPortSize: 36,
-export factories from './src/factories',
-export findAreaFit from './src/utils/findAreaFit',
-export findMinMax from './src/utils/findMinMax',
-export findNodeFit from './src/utils/findNodeFit',
-export mixins from './src/mixins',
-export fromJSON from './src/utils/fromJSON',
+export App from './src/App'
+export Clipboard from './src/Clipboard'
+export Edge from './src/Edge'
+export FONT_AWESOME from './src/config/FONT_AWESOME'
+export Graph from './src/Graph'
+export Group from './src/Group'
+export IIP from './src/Iip'
+export Menu from './src/Menu'
+export ModalBG from './src/ModalBG'
+export Node from './src/Node'
+export NodeMenu from './src/NodeMenu'
+export NodeMenuPort from './src/NodeMenuPort'
+export NodeMenuPorts from './src/NodeMenuPorts'
+export Port from './src/Port'
+export SVGImage from './src/common/SVGImage'
+export TextBG from './src/common/TextBG'
+export Tooltip from './src/Tooltip'
+export arcs from './src/factories/menu/arcs'
+// export config from './src/config'
+export factories from './src/factories'
+export findAreaFit from './src/utils/findAreaFit'
+export findMinMax from './src/utils/findMinMax'
+export findNodeFit from './src/utils/findNodeFit'
+export mixins from './src/mixins'
+export fromJSON from './src/utils/fromJSON'
 /*
-export  nodeRadius: 8,
-export  nodeSide: 56,
-export  nodeSize: 72,
-export  zbpBig: 1.2,
-export  zbpNormal: 0.4,
-export  zbpSmall: 0.01
+export contextPortSize: 36
+export nodeRadius: 8
+export nodeSide: 56
+export nodeSize: 72
+export zbpBig: 1.2
+export zbpNormal: 0.4
+export zbpSmall: 0.01
 export
 */
